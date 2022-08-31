26e547cec882048650a21c68d868a514352413da
Jay <mkvjayanth1@gmail.com>
2022-08-31 18:40:27 +0530

    home page

---
+++ src/pages/Home/Card.js
@@ -0,0 +1,58 @@
+import React from 'react'
+import './Card.css'
+import bitlogo from '../../images/abit.png'
+import bitcoin from '../../images/bitcoin.png'
+import light from '../../images/light.png'
+import gift from '../../images/gift.png'
+
+function Card({ Active }) {
+    console.log('state in card',Active)
+  return (
+    <div className='home__bottomCard'>
+              <div className='home__cardHeader'>
+                <div className='home__cardLogo'>
+                  <img className='home__bitlogo' src={bitlogo} alt='bitlogo' />
+                  <img className='home__bitcoin' src={bitcoin} alt='bitcoin' />
+                </div>
+                <div className='home__cardTitle'>
+                  Tok-EN NAME
+                </div>
+              </div>
+              <div className='home__cardDetails'>
+                <div className='home__cardPercent'>
+                  320.78%
+                </div>
+                <div className='home__cardName'>
+                  APR
+                </div>
+              </div>
+              <div className='home__cardDesc'>
+                <div className='home__descOption'>
+                  <div className='home__descTitle'>Reward Token</div>
+                  <div className='home__descValue'><img src={light} alt='light' /> </div>
+                </div>
+                <div className='home__descOption'>
+                  <div className='home__descTitle'>Value Locked</div>
+                  <div className='home__descValue'>$867,566.11</div>
+                </div>
+                <div className='home__descOption'>
+                  <div className='home__descTitle'>My Share</div>
+                  <div className='home__descValue'>$0(0%)</div>
+                </div>
+                <div className='home__descOption'>
+                  <div className='home__descTitle'>Available Balance</div>
+                  <div className='home__descValue'>$0</div>
+                </div>
+                <div className='home__descOption'>
+                  <div className='home__descTitle'>My Reward</div>
+                  <div className='home__descValue'>$0 <img className='home__descGift' src={gift} alt='gift'/> </div>
+                </div>
+              </div>
+              <div className={'home__cardButton '+(Active ? '' : 'home--ended')}>
+              { Active ? "Stake" : "Ended"}
+              </div>
+            </div>
+  )
+}
+
+export default Card
--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,41 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
 import sandclock from '../../images/sandclock.png';
 import search from '../../images/search.png'
 import './HomePage.css';
 
+import Card from './Card';
+
 function HomePage() {
+
+  const [Active, setActive] = useState(true);
+
   return (
     <div className='HomePage'>
       <div className='home__top'>
         <div className='home__topSub'>
-        <div className='home__topLeft'>
-          <div className='home__topTitle'>Provide Liquidity, Earn FTR</div>
-          <div className='home__topAmount'>$105,786,890.44</div>
-          <div className='home__topDesc'>Total Value Locked(TVL)</div>
-          <div className='home__topSearch'>
-            <div className='home__topSearchBox'>
-              <div className='home__topSearchIcon'>
-                <img src={search} alt='sandclock'/>
+          <div className='home__topLeft'>
+            <div className='home__topTitle'>Provide Liquidity, Earn FTR</div>
+            <div className='home__topAmount'>$105,786,890.44</div>
+            <div className='home__topDesc'>Total Value Locked(TVL)</div>
+            <div className='home__topSearch'>
+              <div className='home__topSearchBox'>
+                <div className='home__topSearchIcon'>
+                  <img src={search} alt='sandclock'/>
+                </div>
+                <div className='home__topSearchInput'>
+                  <input
+                    className='home__topInput'
+                    type='text'
+                    placeholder='Search by token symbol'/>
+                </div>
               </div>
-              <div className='home__topSearchInput'>
-                <input className='home__topInput' type='text' placeholder='Search by token symbol'/>
+              <div className='home__topSearchButton'>
+                Search
               </div>
             </div>
-            <div className='home__topSearchButton'>
-              Search
+          </div>
+          <div className='home__topRight'>
+            <div className='home__topImg'>
+              <img src={sandclock} alt='sandclock'/>
             </div>
           </div>
-        </div>
-        <div className='home__topRight'>
-          <div className='home__topImg'>
-            <img src={sandclock}  alt='sandclock'/>
-          </div>
-        </div>
         </div>
       </div>
       <div className='home__bottom'>
+        <div className='home__bottomSub'>
+          <div className='home__bottomTop'>
+            <div className='home__bottomMenu'>
+            <div className={'home__bottomOption '+(Active ? 'home--active' : '')} onClick={()=>setActive(!Active)}>Active</div>
+              <div className={'home__bottomOption '+(!Active ? 'home--active' : '')} onClick={()=>setActive(!Active)}>Ended</div>
+            </div>
+          </div>
+          <div className='home__bottomGrid'>
+            {Active ?
+              <>
+            <Card Active={ Active} />
+            <Card Active={Active} />
+            <Card Active={ Active} />
+            <Card Active={ Active} />
+            <Card Active={ Active} />
+              </>
+              :
+              <>
+                <Card Active={Active} />
+                <Card Active={Active} />
+                <Card Active={ Active} />
+              </>
+            }
+          </div>
 
-        
+        </div>
       </div>
     </div>
   )
